fd432948690f3523701baacaa2b19c66baa018ec
xiaokeai1987 <158751563+xiaokeai1987@users.noreply.github.com>
2026-02-05 11:15:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1932,7 +1932,7 @@ async function subHtml(request, hostLength = 0, FileName, subProtocol, subConver
     <div class="container">
         <div class="header">
             <div class="social-links-container">
-                <a href="https://github.com/xiaokeai1987/CF-Workers-BPSU" target="_blank" class="social-link" title="BPSUB GitHub 源码仓库">
+                <a href="https://github.com/xiaokeai1987/CF-Workers-BPSUB" target="_blank" class="social-link" title="BPSUB GitHub 源码仓库">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                         <path fill="currentColor" fill-rule="evenodd" d="M7.976 0A7.977 7.977 0 0 0 0 7.976c0 3.522 2.3 6.507 5.431 7.584c.392.049.538-.196.538-.392v-1.37c-2.201.49-2.69-1.076-2.69-1.076c-.343-.93-.881-1.175-.881-1.175c-.734-.489.048-.489.048-.489c.783.049 1.224.832 1.224.832c.734 1.223 1.859.88 2.3.685c.048-.538.293-.88.489-1.076c-1.762-.196-3.621-.881-3.621-3.964c0-.88.293-1.566.832-2.153c-.05-.147-.343-.978.098-2.055c0 0 .685-.196 2.201.832c.636-.196 1.322-.245 2.007-.245s1.37.098 2.006.245c1.517-1.027 2.202-.832 2.202-.832c.44 1.077.146 1.908.097 2.104a3.16 3.16 0 0 1 .832 2.153c0 3.083-1.86 3.719-3.62 3.915c.293.244.538.733.538 1.467v2.202c0 .196.146.44.538.392A7.98 7.98 0 0 0 16 7.976C15.951 3.572 12.38 0 7.976 0" clip-rule="evenodd"></path>
                     </svg>
@@ -2361,7 +2361,7 @@ async function subHtml(request, hostLength = 0, FileName, subProtocol, subConver
         </div>
         
         <div class="footer">
-            <p>© 2025 BPSUB - Powered by Cloudflare Snippets | 感谢白嫖哥提供维护的 - <a href="https://t.me/v2rayByCf" target="_blank" class="thanks-link" title="访问Snipaste节点分享频道">🔗 Snipaste节点</a></p>
+            <p>© 2025 BPSUB - Powered by Cloudflare Snippets
         </div>
     </div>
     
